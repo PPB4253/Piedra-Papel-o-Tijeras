a5bd049c211b7be7e0eaff746694ec1df5a062b3
PPB4253 <adrjimort@alum.us.es>
2024-09-23 12:33:07 +0200

    juego final hasta ejercicio 3

---
+++ src/piedra_papel_tijeras.py
@@ -0,0 +1,36 @@
+import random
+
+def ordenador_decide_jugada():
+    ''' 
+    Elige aleatoriamente entre piedra, papel o tijeras y devuelve la elección.     
+    '''
+    opciones = ["piedra", "papel", "tijeras"]
+    res = random.choice(opciones)
+    return res
+
+def usuario_decide_jugada():
+    ''' 
+    Pide al usuario que elija entre piedra, papel o tijeras y devuelve la elección.     
+    '''
+    eleccion_usuario = input("Elige piedra, papel o tijeras: ")    
+    return eleccion_usuario
+
+def determina_ganador(jugada_usuario, jugada_ordenador):
+    if jugada_usuario == jugada_ordenador:
+        return "Empate"
+    elif jugada_usuario == "piedra" and jugada_ordenador == "tijeras":
+        return "Ganaste"
+    elif jugada_usuario == "tijeras" and jugada_ordenador == "papel":
+        return "Ganaste"
+    elif  jugada_usuario == "papel" and jugada_ordenador == "piedra":
+        return "Ganaste"
+    else:
+        return "Perdiste"
+    
+def jugar():
+    print("Te damos la bienvenida al juego")
+    eleccion_ordenador = ordenador_decide_jugada()
+    eleccion_usuario = usuario_decide_jugada()
+    print("El ordenador eligió: ", eleccion_ordenador)
+    resultado = determina_ganador(eleccion_usuario, eleccion_ordenador)
+    print("Resultado: ", resultado)
